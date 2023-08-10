97e83909d94cc76f5989c88f196b64270ac974b4
bokorni <lucasbokorni@gmail.com>
2023-08-10 11:53:54 -0300

    transition logo

---
+++ script/bokas.js
@@ -0,0 +1,7 @@
+
+function muda_tela(){
+    document.getElementById("btn-more").style.animation = "esconde-more 2s forwards";
+    document.getElementById("logo").style.animation = "some-logo 2s forwards";
+    document.getElementById("logo2").style.animation = "some-logo2 2s forwards";
+    
+}
