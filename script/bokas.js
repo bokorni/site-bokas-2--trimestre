57447e93070c5f1f704d953a082c614a6b8dced6
bokorni <lucasbokorni@gmail.com>
2023-08-17 11:45:02 -0300

    aaaaa

--- script/bokas.js
+++ script/bokas.js
@@ -3,5 +3,6 @@ function muda_tela(){
     document.getElementById("btn-more").style.animation = "esconde-more 2s forwards";
     document.getElementById("logo").style.animation = "some-logo 2s forwards";
     document.getElementById("logo2").style.animation = "some-logo2 2s forwards";
-    
+    document.getElementById("box-noticia").style.display = "grid";
+    document.getElementById("box-noticia").style.animation ="aparece-box 3s forwards";
 }
